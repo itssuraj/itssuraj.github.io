f5e24a1c2b91a9cef3007c77b8aa356007271121
itssuraj <surajksuraj19@gmail.com>
2016-10-12 19:10:18 +0530

    Added full code

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+(function ($) {
+
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$('.navbar-nav li a').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+		$('.page-scroll a').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+})(jQuery);
